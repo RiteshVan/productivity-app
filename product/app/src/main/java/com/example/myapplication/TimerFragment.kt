c16ec459b280a523218280d5274bde0a7843f76e
unknown <naga.reddyvangala.2022@live.rhul.ac.uk>
2024-12-03 19:02:24 +0000

    Added drawable and font files needed and implemented new look timer page

--- product/app/src/main/java/com/example/myapplication/TimerFragment.kt
+++ product/app/src/main/java/com/example/myapplication/TimerFragment.kt
@@ -16,14 +16,7 @@ import android.widget.TextView
 
 class TimerFragment : Fragment() {
 
-    private lateinit var spinner: Spinner
-    private lateinit var countDownTimer: CountDownTimer
-    private var timerRunning = false
-
-
-    private lateinit var textViewTimer: TextView
-    private lateinit var progressBar: ProgressBar
-    private lateinit var stopStartPause: Button
+    
 
 
     override fun onCreateView(
@@ -31,23 +24,7 @@ class TimerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_timer, container, false)
-        textViewTimer = view.findViewById(R.id.timeLeft)
-        progressBar = view.findViewById(R.id.time_progress)
-        stopStartPause = view.findViewById(R.id.start_stop_button)
 
-
-        spinner = view.findViewById(R.id.spinner_time)
-
-        val listItems = listOf(15,25,30,45,60)
-
-        val arrayAdapter = ArrayAdapter(requireContext(),android.R.layout.simple_spinner_item,listItems)
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item)
-        spinner.adapter = arrayAdapter
-
-        val timeChosen= spinner.selectedItem
-
-
-        progressBar.max = 100
 
 
 
