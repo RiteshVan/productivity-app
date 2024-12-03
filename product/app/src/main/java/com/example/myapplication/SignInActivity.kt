0722d9277fbcaa34cdf17156ead5585ee9e42e35
unknown <naga.reddyvangala.2022@live.rhul.ac.uk>
2024-12-03 20:05:19 +0000

    Added functionality for sign in page

--- product/app/src/main/java/com/example/myapplication/SignInActivity.kt
+++ product/app/src/main/java/com/example/myapplication/SignInActivity.kt
@@ -1,20 +1,51 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import javax.security.auth.callback.PasswordCallback
 
+//Activity wheres users can login with their details and proceed to main application
 class SignInActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
+        //Sets layout for page
         setContentView(R.layout.activity_sign_in)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        val signIn = findViewById<ImageButton>(R.id.sign_in)
+
+
+
+        //Listener for when user clicks button. User is then directed to HomeActivity, which hold the app fragments
+        signIn.setOnClickListener{
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
+
+
+
+
+
 }
