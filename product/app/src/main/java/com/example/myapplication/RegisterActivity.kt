3d3427c9b802558876174194fe6e36a7a9907c63
unknown <naga.reddyvangala.2022@live.rhul.ac.uk>
2024-12-03 19:58:26 +0000

    Functionality for registration page added

--- product/app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ product/app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -1,20 +1,48 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+//Register page where users can sign up or move to sign in screen
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
+        //Sets the layout for the register page
         setContentView(R.layout.activity_register)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        //Assigns sign in image button to variable
+        val signInButton = findViewById<ImageButton>(R.id.registered_sign_in)
+
+
+        //Listener checks for click and redirects user to sign in page
+        signInButton.setOnClickListener{
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
+
+
 }
