04698d7e65d94b4b822bdd26ad225d215f12961b
unknown <naga.reddyvangala.2022@live.rhul.ac.uk>
2024-12-03 19:56:16 +0000

    Added sign up page

--- product/app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ product/app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -1,38 +1,20 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-//Start point of application
-class MainActivity : AppCompatActivity() {
-
-
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
-
-        //Sets layout for opening page
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        //Assigns image button to variable so it can be used in listener
-        val startButton = findViewById<ImageButton>(R.id.startButton)
-
-        //When button is clicked the user is sent to the register page of the app
-        startButton.setOnClickListener{
-            val intent = Intent(this,RegisterActivity::class.java)
-            startActivity(intent)
         }
     }
 }
