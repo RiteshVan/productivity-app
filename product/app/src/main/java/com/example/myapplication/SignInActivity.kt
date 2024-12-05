f1b8483f456de0fecaeddbb18c2ebc7a8910cd19
unknown <naga.reddyvangala.2022@live.rhul.ac.uk>
2024-12-05 11:47:19 +0000

    Updated sign in page to use backend sign in system

--- product/app/src/main/java/com/example/myapplication/SignInActivity.kt
+++ product/app/src/main/java/com/example/myapplication/SignInActivity.kt
@@ -9,11 +9,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import javax.security.auth.callback.PasswordCallback
+
 
 //Activity wheres users can login with their details and proceed to main application
 class SignInActivity : AppCompatActivity() {
 
+    private lateinit var db:LoginDetailsDatabase
+
+    private lateinit var usernameText: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -30,6 +33,7 @@ class SignInActivity : AppCompatActivity() {
             insets
         }
 
+        db = LoginDetailsDatabase(this)
 
         val signIn = findViewById<ImageButton>(R.id.sign_in)
 
@@ -37,14 +41,31 @@ class SignInActivity : AppCompatActivity() {
 
         //Listener for when user clicks button. User is then directed to HomeActivity, which hold the app fragments
         signIn.setOnClickListener{
-            val intent = Intent(this,HomeActivity::class.java)
-            startActivity(intent)
+            val username = findViewById<EditText>(R.id.username_input)
+            val password = findViewById<EditText>(R.id.password_input)
+
+            usernameText = username.text.toString()
+
+            login(usernameText,password.text.toString())
         }
 
     }
 
 
+    private fun login(username:String,password:String) {
+        val userIsPresent = db.verifyUser(username,password)
 
+        if (userIsPresent) {
+            Toast.makeText(this,"Login successful!",Toast.LENGTH_SHORT).show()
+            val intent = Intent(this,HomeActivity::class.java)
+            intent.putExtra("Username",username)
+            startActivity(intent)
+            finish()
+        }
+        else{
+            Toast.makeText(this,"Login unsuccessful!",Toast.LENGTH_SHORT).show()
+        }
+    }
 
 
 
