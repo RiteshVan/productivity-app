6cfff44c345b22a08456b7449a1a7d11f0552eff
Reddy Vangala Naga (2022) ZLAC339 <naga.reddyvangala.2022@live.rhul.ac.uk>
2025-01-27 21:45:25 +0000

    added functionality to retrieve tasks

--- backend_files/tasks_backend.py
+++ backend_files/tasks_backend.py
@@ -12,7 +12,7 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     tag = db.Column(db.String(25), nullable=False)
-    hours_worked = db.Column(db.Integer, nullable=False)
+    hours = db.Column(db.Integer, nullable=False)
     username = db.Column(db.String(25), nullable=False)
 
 
@@ -21,23 +21,39 @@ class Task(db.Model):
 def test(): 
     return "Task backend running"
 
-
 #Functionality to add task
 @tasks_backend.route("/add_task",methods=['POST'])
 def add_task():
+    id = request.form["id"]
     title = request.form["title"]
     tag = request.form["tag"]
-    hours_worked = request.form["hours_worked"]
+    hours = request.form["hours"]
     username = request.form["username"]
     
     
-    new_task= Task(title=title,tag=tag,hours_worked=hours_worked,username=username)
+    new_task= Task(id=id,title=title,tag=tag,hours=hours,username=username)
     
     db.session.add(new_task)
     db.session.commit()
     
     return "Successfully added task"
 
+@tasks_backend.route("/get_tasks")
+def get_tasks():
+    tasks = Task.query.all()
+    task_list = [{
+
+        'id':task.id,
+        'title':task.title,
+        'tag':task.tag,
+        'hours':task.hours,
+        'username':task.username
+
+    }
+    for task in tasks
+    ]
+
+    return {"tasks":task_list}
 
 if __name__ == '__main__':
     with tasks_backend.app_context():
