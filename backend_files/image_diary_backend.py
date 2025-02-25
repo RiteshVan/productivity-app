a58f1e7530dbd1a7fe3d33179c13ec595837c3ee
Reddy Vangala Naga (2022) ZLAC339 <naga.reddyvangala.2022@live.rhul.ac.uk>
2025-02-25 20:15:46 +0000

    image diary feature backend added to stores images with task details as their captions

---
+++ backend_files/image_diary_backend.py
@@ -0,0 +1,47 @@
+from flask import Flask,request,render_template
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+import os
+
+
+image_diary_backend = Flask(__name__)
+image_diary_backend.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///images.db'
+image_diary_backend.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+image_diary_backend.config['UPLOAD_FOLDER'] = 'uploads'
+
+db =  SQLAlchemy(image_diary_backend)
+
+os.makedirs(image_diary_backend.config['UPLOAD_FOLDER'], exist_ok=True)
+
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String(150), nullable=False)
+    image = db.Column(db.String(150),nullable=False)
+
+
+@image_diary_backend.route('/test')
+def test():
+    return 'Image Backend Operational'
+
+@image_diary_backend.route('/upload', methods=['POST'])
+def upload():
+    image = request.files['image']
+    caption = request.form['caption']
+
+    if image:
+        image_name = secure_filename(image.filename)
+        image_path = os.path.join(image_diary_backend.config['UPLOAD_FOLDER'],image_name)
+
+        image.save(image_path)
+
+        new_image = Image(image=image_name,caption=caption)
+        db.session.add(new_image)
+        db.session.commit()
+
+    return "added image"
+
+
+if __name__ == '__main__':
+    with image_diary_backend.app_context():
+        db.create_all()
+    image_diary_backend.run(host='0.0.0.0',port=4997)
