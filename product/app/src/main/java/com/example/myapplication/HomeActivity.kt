46ecca568258fe887ebd1129816e20d3a7c16672
DESKTOP-I9VPMGD\Ritesh <naga.reddyvangala.2022@live.rhul.ac.uk>
2025-03-30 13:40:35 +0100

    updated home activity

--- product/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ product/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -2,94 +2,118 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.databinding.ActivityHomeBinding
 import com.google.android.material.navigation.NavigationView
 
-//Main screen of the app with navigation to move to different feature pages
-class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener{
-
+// Main screen of the app with navigation to move to different feature pages
+class HomeActivity :
+    AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
     private lateinit var fragmentManager: FragmentManager
     private lateinit var binding: ActivityHomeBinding
+    private var username: String? = null // Gets the username from the sign in page
 
-
-
-    //Activity is initialised
+    // Activity is initialised
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        username = intent.getStringExtra("username")
 
-        //Listener to identify which item is selected by user
+        // Listener to identify which item is selected by user
         binding.navigationDrawer.setNavigationItemSelectedListener(this)
-        binding.bottomNav.background=null
+        binding.bottomNav.background = null
 
-
-        //Directs user to chosen fragment
+        // Directs user to chosen fragment
         binding.bottomNav.setOnItemSelectedListener { item ->
-            when(item.itemId){
+            when (item.itemId) {
+                R.id.home_nav_button -> startHomeFragment()
 
-                R.id.home_nav_button->startHomeFragment()
-               // R.id.timer_nav_button-> startFragment(TimerFragment())
-                R.id.setting_nav_button -> startFragment(TagsFragment())
+                R.id.timer_nav_button -> startFragment(TimerFragment())
                 R.id.tasks_nav_button -> startTaskFragment()
-                R.id.diary-> startFragment(ImageDiaryFragment())
+                R.id.diary -> startImageDiary()
                 R.id.leaderboard_nav_button -> startFragment(LeaderboardFragment())
-
-
             }
             true
-
         }
         fragmentManager = supportFragmentManager
 
-
-        //By default opens HomeFragment
+        // By default opens HomeFragment
         startHomeFragment()
-
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
+        TODO("Not required")
     }
 
+    /*Function used to contain the process of switching features so that code can be
+      more easy to read and simpler to implement*/
+    private fun startFragment(fragment: Fragment) {
+        val bundle = Bundle()
+        bundle.putString("username", username)
+        fragment.arguments = bundle
 
-
-    //Function used to contain the process of switching features so that code can be more easy to read and simpler to implement
-    private fun startFragment(fragment: Fragment){
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container,fragment)
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
         fragmentTransaction.commit()
-
     }
 
-    private fun startTaskFragment(){
+    /**
+     * This function is used to open the tasks fragment.
+     * Username is passed so that tasks shown are relevant to the user
+     */
+    private fun startTaskFragment() {
         val tasksFragment = TasksFragment()
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,tasksFragment)
+        tasksFragment.arguments =
+            Bundle().apply {
+                putString("username", username)
+            }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, tasksFragment)
             .commit()
-
-
-
     }
 
-
+    /**
+     * This function is used to open the home fragment.
+     * Username is passed so that chat shown is relevant to user
+     */
     private fun startHomeFragment() {
         val homeFragment = HomeFragment()
 
+        homeFragment.arguments =
+            Bundle().apply {
+                putString("username", username)
+            }
 
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,homeFragment)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, homeFragment)
             .commit()
-
-
     }
 
+    /**
+     * This function is used to open the image diary fragment.
+     * Username is passed so that images shown are relevant to user
+     */
+    private fun startImageDiary() {
+        val imageDiaryFragment = ImageDiaryFragment()
+
+        imageDiaryFragment.arguments =
+            Bundle().apply {
+                putString("username", username)
+            }
+
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, imageDiaryFragment)
+            .commit()
+    }
 }
