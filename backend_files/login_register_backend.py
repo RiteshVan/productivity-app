becd71dc6bdaaa3d428d077b2fe4e29b9f121904
Reddy Vangala Naga (2022) ZLAC339 <naga.reddyvangala.2022@live.rhul.ac.uk>
2025-03-31 19:30:28 +0000

    Adds comments and makes rate limits more reasonable

--- backend_files/login_register_backend.py
+++ backend_files/login_register_backend.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+#initialise application and database settings
 login_register_backend =  Flask(__name__)
 
 login_register_backend.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login_register_backend.db'
@@ -11,13 +12,15 @@ login_register_backend.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(login_register_backend)
 
+#used to stop excessive requests and prevent denial of servcice attacks 
 limiter = Limiter(
     app=login_register_backend,
     key_func=get_remote_address,
-    default_limits=["100 per hour","7 per minute"]
+    default_limits=["100 per hour","30 per minute"]
 
 )
 
+#user table is defined
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), primary_key=False)
@@ -31,20 +34,32 @@ def test():
 #Register users
 @login_register_backend.route('/register', methods=['POST'])
 def register():
+    #details to be recieved from app
     username = request.form['username']
     password = request.form['password']
 
+    #checks if username already exists before allowing registration
     user_exists = User.query.filter_by(username=username).first()
 
+    #converted to bytes to be readied for encryption
     password_bytes = password.encode('utf-8')
 
+    #salt used for extra security
     salt = bcrypt.gensalt(rounds=14)
 
+    #password hashed before being stores
     hashed_password = bcrypt.hashpw(password_bytes,salt)
 
+
+    #ensures username is valid
     if user_exists:
         return "Username taken"
     
+    #ensures username is not empty
+    elif (username==""): 
+        return "Empty username"
+    
+    #if valid the user is added to the database and confirmation message is shown
     else:
         new_user = User(username=username, password=hashed_password)
 
@@ -57,25 +72,33 @@ def register():
 #Checks user details then logs them in
 @login_register_backend.route('/login', methods=['POST'])
 def login():
+    #details to be recieved from app
     username = request.form['username']
     password = request.form['password']
 
+    #checks to see if user is present on the database
     existing_user = User.query.filter_by(username=username).first()
 
+    #password converted to bytes to be checked against hash stored in database
     password_bytes = password.encode('utf-8')
 
+    #hash in database
     check_password_bytes = existing_user.password
 
-
+    #if user exists then check
     if existing_user:
+        #is pasword is correct log users in
         if bcrypt.checkpw(password_bytes,check_password_bytes):
             return "Logged in successfully"
+        #let user know username/password (password is the wrong one, but do not specify for security purpose)
         else:
-            return "Incorrect username and password combination"    
+            return "Incorrect username and password combination"        
+    #Let user know username does not exist
     else:
         return "User does not exist"
     
 
+#to run
 if __name__ == '__main__':
     with login_register_backend.app_context():
         db.create_all()
